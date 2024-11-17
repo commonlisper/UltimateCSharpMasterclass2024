1b3787deb30675368b5bc0f75763009655e7c0f7
Common Lisper <atosdo@live.com>
2024-11-17 20:20:03 +0200

    Add project files.

---
+++ TodoList/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+PrintMessageFromUser("Hello, World!");
+
+Console.ReadKey();
+
+void PrintMessageFromUser(string message)
+{
+    Console.WriteLine(message);
+}
